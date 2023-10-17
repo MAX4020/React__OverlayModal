26b5c713fb0d7702ddf425b812a7ffc9e3f4ca14
MAX4020 <144027580+MAX4020@users.noreply.github.com>
2023-10-17 21:36:23 +0400

    3

--- app/src/Modal.jsx
+++ app/src/Modal.jsx
@@ -1,32 +1,13 @@
 import React, {Comment, useEffect, useMemo} from "react"
 import { ReactDOM } from "react"
 import {createPortal} from "react-dom"
-
-const modalRootElement = document.querySelector('#modal')
-
 export const Modal = (props) => {
-  const{marker, open, onClose} = props;
-
-  const el = useMemo(()=>{
-    const el = document.createElement("div")
-    el.dataset.marker = marker
-    return el
-  },[marker])
-
-  useEffect(()=>{
-    if(open){      
-      modalRootElement.appendChild(el)
-
-    return () => {
-      modalRootElement.removeChild(el)}
-    }
-
-  })
+  const{open, onClose} = props;
 
   if(open){
       return createPortal(
-      <div onClick={onClose} className="w-full h-full m-auto absolute left-0 right-0 top-0"><ModalInfo>{props.children}</ModalInfo></div> ,
-    el
+      <ModalInfo onClose={onClose}>{props.children}</ModalInfo>,
+    document.body
    );}
 
 
@@ -34,7 +15,7 @@ export const Modal = (props) => {
 
 export const Overlay = ({children, onClose}) => {
   return ( 
-    <div className="bg-slate-800/[.50] w-full h-full m-auto absolute left-0 right-0 top-0">{children}</div>
+    <div onClick={onClose} className="bg-slate-800/[.50] w-full h-full m-auto absolute left-0 right-0 top-0">{children}</div>
    );
 }
 
@@ -55,12 +36,14 @@ export const Btn = ({text}) => {
 }
 
 export const ModalInfo = (props) => {
-  const {children, open, onClose} = props
+  const {children, onClose} = props
   return ( 
     <>
-    <Overlay open={open} onClick={onClose}>
-      <div className="w-1/3 m-auto  bg-emerald-800"><Header title={"ModalInfo"}/></div>
-      <div className="w-1/3 h-96 flex justify-center m-auto overflow-y-scroll bg-emerald-700">{children}</div>
+    <Overlay onClose={onClose}>
+      <div onClick={(e)=>e.stopPropagation()} className="w-1/3 m-auto  bg-emerald-800">
+        <Header title={"ModalInfo"}/>
+        <div className="h-96 flex justify-center m-auto overflow-y-scroll bg-emerald-700">{children}</div>
+        </div>
     </Overlay>
     </>
    );
